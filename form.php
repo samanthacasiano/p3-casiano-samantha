3419a111d5ae54c9283bca01ba8b5f802a7533ce
Samantha Casiano <samanthacasiano@gmail.com>
2021-04-27 23:16:31 -0400

    update lightbox html on contact page

---
+++ form.php
@@ -0,0 +1,23 @@
+<!-- HTML top code goes here -->
+
+<?
+
+// From processing takes place here. message displayed here too
+require('form/form.class.php');
+$form = new Form();
+$form->processForm(
+
+	'samanthacasiano.com' // Put you mail domain here
+	,
+	'For the Goal-Getters' // Put your site name / form name here
+	,
+	'samanthacasiano@ufl.edu' // Where will the form notification be sent?
+	,
+	'noreply@samanthacasiano.com' // This what the form FROM: address wil be, if the form submissions doesn't contain an email field
+
+);
+
+
+?>
+
+<!-- HTML bottom code goes here -->
